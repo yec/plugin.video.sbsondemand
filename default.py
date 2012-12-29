a5bf44ca4d1a6e23c83566af7e6c9f70eed1c355
Ye Chuah <yechuah@gmail.com>
2012-12-29 22:23:11 +1100

    can list show categories

--- default.py
+++ default.py
@@ -2,9 +2,10 @@ import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmcaddon,xbmc,json,sys,os
 import urlparse
 
 _curDir = os.path.dirname(__file__)
-sys.path.append(_curDir + '/resources/lib/sbsOnDemand')
+sys.path.append(_curDir + '/resources/lib/sbsOnDemand/')
 
 import SbsOnDemand.config
+import SbsOnDemand.Feed
 
 _thisPlugin = int(sys.argv[1])
 _scheme = 'plugin://plugin.video.sbsondemand/'
@@ -15,15 +16,31 @@ def routes(url):
 
     if path == '':
         view_feeds(url)
+    elif re.search('category/.*', path):
+        view_shows(path)
 
 def view_feeds(url):
     global _thisPlugin
 
+    a = [{ 'name' : 'Programs', 'feedId': 'section-programs' }]
     for item in SbsOnDemand.config.DEFAULT_FEEDS:
         li = xbmcgui.ListItem(item['name'])
         isfolder = True
         xbmcplugin.addDirectoryItem(_thisPlugin, url + 'category/' + item['feedId'], li, isfolder)
 
     xbmcplugin.endOfDirectory(_thisPlugin)
+
+def view_shows(url):
+    global _thisPlugin
+
+    m = re.search('category/(.+)', url)
+    feedname = m.group(1)
+    feed = SbsOnDemand.Feed.getFeedFromId(feedname)
+    for video in feed.getVideos():
+        li = xbmcgui.ListItem(video.title)
+        xbmcplugin.addDirectoryItem(_thisPlugin, video.url, li)
+
+    xbmcplugin.endOfDirectory(_thisPlugin)
+
 
 routes(sys.argv[0])
